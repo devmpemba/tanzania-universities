eefecdf11114e455e0f4956f155d8a8c761f4164
Salym Mbise <salymdev@gmail.com>
2025-04-15 09:18:58 +0300

    first commit

---
+++ src/UniversityList.php
@@ -0,0 +1,245 @@
+<?php
+
+namespace SalymMbise\Universities;
+
+class UniversityList
+{
+    public static function all()
+    {
+        return [  
+            [  
+                'name' => 'University of Dar es Salaam',  
+                'abbreviation' => 'UDSM',  
+                'type' => 'Public',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'https://www.udsm.ac.tz'  
+            ],  
+            [  
+                'name' => 'Ardhi University',  
+                'abbreviation' => 'ARU',  
+                'type' => 'Public',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'https://www.aru.ac.tz'  
+            ],  
+            [  
+                'name' => 'Nelson Mandela African Institute of Science and Technology',  
+                'abbreviation' => 'NM-AIST',  
+                'type' => 'Public',  
+                'location' => 'Arusha',  
+                'website' => 'https://www.nm-aist.ac.tz'  
+            ],  
+            [  
+                'name' => 'St. Augustine University of Tanzania',  
+                'abbreviation' => 'SAUT',  
+                'type' => 'Private',  
+                'location' => 'Mwanza',  
+                'website' => 'https://www.saut.ac.tz'  
+            ],  
+            [  
+                'name' => 'Muhimbili University of Health and Allied Sciences',  
+                'abbreviation' => 'MUHAS',  
+                'type' => 'Public',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'https://www.muhas.ac.tz'  
+            ],  
+            [  
+                'name' => 'University of Dodoma',  
+                'abbreviation' => 'UDOM',  
+                'type' => 'Public',  
+                'location' => 'Dodoma',  
+                'website' => 'https://www.udom.ac.tz'  
+            ],  
+            [  
+                'name' => 'Open University of Tanzania',  
+                'abbreviation' => 'OUT',  
+                'type' => 'Public',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'https://www.out.ac.tz'  
+            ],  
+            [  
+                'name' => 'Ruaha Catholic University',  
+                'abbreviation' => 'RuCU',  
+                'type' => 'Private',  
+                'location' => 'Iringa',  
+                'website' => 'https://www.rucu.ac.tz'  
+            ],  
+            [  
+                'name' => 'University of Bagamoyo',  
+                'abbreviation' => 'UoB',  
+                'type' => 'Private',  
+                'location' => 'Bagamoyo',  
+                'website' => 'https://www.uob.ac.tz'  
+            ],  
+            [  
+                'name' => 'Moshi Cooperative University',  
+                'abbreviation' => 'MoCU',  
+                'type' => 'Public',  
+                'location' => 'Moshi',  
+                'website' => 'https://www.mocu.ac.tz'  
+            ],  
+            [  
+                'name' => 'Institute of Finance Management',  
+                'abbreviation' => 'IFM',  
+                'type' => 'Public',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'http://www.ifm.ac.tz'  
+            ],  
+            [  
+                'name' => 'National Institute of Transport',  
+                'abbreviation' => 'NIT',  
+                'type' => 'Public',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'https://www.nit.ac.tz'  
+            ],  
+            [  
+                'name' => 'Arusha Technical College',  
+                'abbreviation' => 'ATC',  
+                'type' => 'Public',  
+                'location' => 'Arusha',  
+                'website' => 'http://www.atc.ac.tz'  
+            ],  
+            [  
+                'name' => 'Dar es Salaam Institute of Technology',  
+                'abbreviation' => 'DIT',  
+                'type' => 'Public',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'http://www.dit.ac.tz'  
+            ],  
+            [  
+                'name' => 'Mbeya University of Science and Technology',  
+                'abbreviation' => 'MUST',  
+                'type' => 'Public',  
+                'location' => 'Mbeya',  
+                'website' => 'https://www.must.ac.tz'  
+            ],  
+            [  
+                'name' => 'Tanzania Institute of Accountancy',  
+                'abbreviation' => 'TIA',  
+                'type' => 'Public',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'http://www.tia.ac.tz'  
+            ],  
+            [  
+                'name' => 'University of the People',  
+                'abbreviation' => 'UoPeople',  
+                'type' => 'Online',  
+                'location' => 'N/A',  
+                'website' => 'https://www.uopeople.edu'  
+            ],  
+            [  
+                'name' => 'Northeastern University (Tanzania)',  
+                'abbreviation' => 'NEU',  
+                'type' => 'Private',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'https://www.neu.ac.tz'  
+            ],  
+            [  
+                'name' => 'Islamic University of Tanzania',  
+                'abbreviation' => 'IUT',  
+                'type' => 'Private',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'http://www.iut.ac.tz'  
+            ],  
+            [  
+                'name' => 'Kilimanjaro Christian Medical University College',  
+                'abbreviation' => 'KCMUCo',  
+                'type' => 'Private',  
+                'location' => 'Moshi',  
+                'website' => 'https://www.kcmuco.ac.tz'  
+            ],  
+            [  
+                'name' => 'Pioneer International University',  
+                'abbreviation' => 'PIU',  
+                'type' => 'Private',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'https://www.piu.ac.tz'  
+            ],  
+            [  
+                'name' => 'Christian Social Services Commission - College of Health and Allied Sciences',  
+                'abbreviation' => 'CSSC',  
+                'type' => 'Private',  
+                'location' => 'Dodoma',  
+                'website' => 'https://www.cssc.ac.tz'  
+            ],  
+            [  
+                'name' => 'Ruaha Catholic University (RUCU)',  
+                'abbreviation' => 'RUCU',  
+                'type' => 'Private',  
+                'location' => 'Iringa',  
+                'website' => 'https://www.rucu.ac.tz'  
+            ],  
+            [  
+                'name' => 'Sokoine University of Agriculture',  
+                'abbreviation' => 'SUA',  
+                'type' => 'Public',  
+                'location' => 'Morogoro',  
+                'website' => 'https://www.sua.ac.tz'  
+            ],  
+            [  
+                'name' => 'University of Iringa',  
+                'abbreviation' => 'UoI',  
+                'type' => 'Private',  
+                'location' => 'Iringa',  
+                'website' => 'http://www.uoi.ac.tz'  
+            ],  
+            [  
+                'name' => 'St. Augustine University of Tanzania (SAUT)',  
+                'abbreviation' => 'SAUT',  
+                'type' => 'Private',  
+                'location' => 'Mwanza',  
+                'website' => 'https://www.saut.ac.tz'  
+            ],  
+            [  
+                'name' => 'Tumaini University Makumira',  
+                'abbreviation' => 'TUMA',  
+                'type' => 'Private',  
+                'location' => 'Arusha',  
+                'website' => 'http://www.tumaini.ac.tz'  
+            ], 
+            
+            [  
+                'name' => 'The Hubert Kairuki Memorial University',  
+                'abbreviation' => 'HKMU',  
+                'type' => 'Private',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'http://www.hkmu.ac.tz'  
+            ],  
+            [  
+                'name' => 'St. Joseph University in Tanzania',  
+                'abbreviation' => 'SJUIT',  
+                'type' => 'Private',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'http://www.sjuit.ac.tz'  
+            ],  
+
+            [  
+                'name' => 'Northeastern University (Tanzania)',  
+                'abbreviation' => 'NEU',  
+                'type' => 'Private',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'https://www.neu.ac.tz'  
+            ],  
+            [  
+                'name' => 'College of Arts and Social Sciences',  
+                'abbreviation' => 'CASS',  
+                'type' => 'Public',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'http://www.udsm.ac.tz'  
+            ],  
+            [  
+                'name' => 'College of Natural and Applied Sciences',  
+                'abbreviation' => 'CONAS',  
+                'type' => 'Public',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'http://www.udsm.ac.tz'  
+            ],  
+            [  
+                'name' => 'University of Science and Technology of Tanzania',  
+                'abbreviation' => 'UST',  
+                'type' => 'Private',  
+                'location' => 'Dar es Salaam',  
+                'website' => 'http://www.ust.ac.tz'  
+            ], 
+        ];  
+    }
+}
